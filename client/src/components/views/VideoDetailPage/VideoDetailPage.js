11e02b3734fe9a90647762d31033874f9ec273a1
daein <dnfwlxo11@naver.com>
2021-05-13 13:28:12 +0900

    add: video detail page side video space

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { List, Avatar, Row, Col } from 'antd';
 import axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 function VideoDetailPage(props) {
 
@@ -20,8 +21,6 @@ function VideoDetailPage(props) {
                     alert('비디오 정보 로딩 실패');
                 }
             });
-
-        return () => (mounted = false);
     }, []);
 
     if (Video.writer) {
@@ -30,6 +29,7 @@ function VideoDetailPage(props) {
             <Row gutter>
                 <Col lg={18} xs={24}>
                     <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
+                        
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${Video.filePath}`} controls />
                         <List.Item
 
@@ -48,38 +48,15 @@ function VideoDetailPage(props) {
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
-                    {/* side video */}
-                    {/* <SideVideo /> */}
+                    <SideVideo />
                 </Col>
             </Row>
         )
     } else {
         return (
-            <Row gutter >
-                <Col lg={18} xs={24}>
-                    <div className="postPage" style={{ width: '100%', padding: '3rem 4em' }}>
-                        <video style={{ width: '100%' }} controls />
-                        {/* <List.Item
-                            
-                            actions={[<LikeDislikes video videoId={videoId} userId={localStorage.getItem('userId')}  />, <Subscriber userTo={Video.writer._id} userFrom={localStorage.getItem('userId')} />]}
-                        >
-                            <List.Item.Meta
-                                avatar={<Avatar />}
-                                title='test'
-                                description='test'
-                            />
-                            <div></div>
-                        </List.Item> */}
-
-                        {/* <Comments CommentLists={CommentLists} postId={Video._id} refreshFunction={updateComment} /> */}
-
-                    </div>
-                </Col>
-                <Col lg={6} xs={24}>
-                    {/* side video */}
-                    {/* <SideVideo /> */}
-                </Col>
-            </Row>
+            <div>
+                Loading...
+            </div>
         )
     }
 }
